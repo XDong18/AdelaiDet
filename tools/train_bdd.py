c9fb46207f84d7b514b3b8340290227ba68bdd2d
Xudong Liu <xudongliu@XudongdeMacBook-Pro.local>
2020-12-28 16:15:52 +0800

    cc

---
+++ tools/train_bdd.py
@@ -0,0 +1,185 @@
+import logging
+import os
+from collections import OrderedDict
+import torch
+
+import detectron2.utils.comm as comm
+from detectron2.checkpoint import DetectionCheckpointer
+from detectron2.config import get_cfg
+from detectron2.data import MetadataCatalog, DatasetCatalog
+from detectron2.engine import DefaultTrainer, default_argument_parser, default_setup, hooks, launch
+from detectron2.evaluation import COCOEvaluator
+from detectron2.modeling import GeneralizedRCNNWithTTA
+from detectron2 import model_zoo
+from detectron2.data.datasets import register_coco_instances
+from detectron2.utils.logger import setup_logger
+
+from adet.data.dataset_mapper import DatasetMapperWithBasis
+from adet.config import get_cfg
+from adet.checkpoint import AdetCheckpointer
+from adet.evaluation import TextEvaluator
+
+
+class Trainer(DefaultTrainer):
+    """
+    We use the "DefaultTrainer" which contains pre-defined default logic for
+    standard training workflow. They may not work for you, especially if you
+    are working on a new research project. In that case you can write your
+    own training loop. You can use "tools/plain_train_net.py" as an example.
+    """
+    @classmethod
+    def build_evaluator(cls, cfg, dataset_name, output_folder=None):
+        if output_folder is None:
+            output_folder = os.path.join(cfg.OUTPUT_DIR, "inference_val")
+        return COCOEvaluator(dataset_name, cfg, True, output_folder)
+
+class Trainer(DefaultTrainer):
+    """
+    This is the same Trainer except that we rewrite the
+    `build_train_loader`/`resume_or_load` method.
+    """
+    def resume_or_load(self, resume=True):
+        if not isinstance(self.checkpointer, AdetCheckpointer):
+            # support loading a few other backbones
+            self.checkpointer = AdetCheckpointer(
+                self.model,
+                self.cfg.OUTPUT_DIR,
+                optimizer=self.optimizer,
+                scheduler=self.scheduler,
+            )
+        super().resume_or_load(resume=resume)
+
+    def train_loop(self, start_iter: int, max_iter: int):
+        """
+        Args:
+            start_iter, max_iter (int): See docs above
+        """
+        logger = logging.getLogger("adet.trainer")
+        logger.info("Starting training from iteration {}".format(start_iter))
+
+        self.iter = self.start_iter = start_iter
+        self.max_iter = max_iter
+
+        with EventStorage(start_iter) as self.storage:
+            self.before_train()
+            for self.iter in range(start_iter, max_iter):
+                self.before_step()
+                self.run_step()
+                self.after_step()
+            self.after_train()
+
+    def train(self):
+        """
+        Run training.
+
+        Returns:
+            OrderedDict of results, if evaluation is enabled. Otherwise None.
+        """
+        self.train_loop(self.start_iter, self.max_iter)
+        if hasattr(self, "_last_eval_results") and comm.is_main_process():
+            verify_results(self.cfg, self._last_eval_results)
+            return self._last_eval_results
+
+    @classmethod
+    def build_train_loader(cls, cfg):
+        """
+        Returns:
+            iterable
+
+        It calls :func:`detectron2.data.build_detection_train_loader` with a customized
+        DatasetMapper, which adds categorical labels as a semantic mask.
+        """
+        mapper = DatasetMapperWithBasis(cfg, True)
+        return build_detection_train_loader(cfg, mapper=mapper)
+
+    @classmethod
+    def build_evaluator(cls, cfg, dataset_name, output_folder=None):
+        if output_folder is None:
+            output_folder = os.path.join(cfg.OUTPUT_DIR, "inference_val")
+        return COCOEvaluator(dataset_name, cfg, True, output_folder
+
+    @classmethod
+    def test_with_TTA(cls, cfg, model):
+        logger = logging.getLogger("adet.trainer")
+        # In the end of training, run an evaluation with TTA
+        # Only support some R-CNN models.
+        logger.info("Running inference with test-time augmentation ...")
+        model = GeneralizedRCNNWithTTA(cfg, model)
+        evaluators = [
+            cls.build_evaluator(
+                cfg, name, output_folder=os.path.join(cfg.OUTPUT_DIR, "inference_TTA")
+            )
+            for name in cfg.DATASETS.TEST
+        ]
+        res = cls.test(cfg, model, evaluators)
+        res = OrderedDict({k + "_TTA": v for k, v in res.items()})
+        return res
+
+def setup(args):
+    """
+    Create configs and perform basic setups.
+    """
+    cfg = get_cfg()
+
+    # set config file
+    cfg.merge_from_file("configs/CondInst/MS_R_50_3x.yaml")
+    cfg.DATASETS.TRAIN = ("bdd100k_train",)
+    cfg.DATASETS.TEST = ("bdd100k_test",)
+    # cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-Detection/faster_rcnn_X_101_32x8d_FPN_3x.yaml")  # Let training initialize from model zoo
+
+
+    cfg.OUTPUT_DIR = './out/condinst_r_50_3x'
+    os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
+    # new added solver arguments
+    cfg.SOLVER.CHECKPOINT_PERIOD = 500
+    cfg.TEST.EVAL_PERIOD = 500
+    # end of new arguments
+
+    # cfg.merge_from_file(args.config_file)
+    cfg.merge_from_list(args.opts)
+    cfg.freeze()
+    default_setup(cfg, args)
+    rank = comm.get_rank()
+    setup_logger(cfg.OUTPUT_DIR, distributed_rank=rank, name="adet")
+    return cfg
+
+
+def main(args):
+    cfg = setup(args)
+    # dataset
+    register_coco_instances("bdd100k_train", {}, "/shared/xudongliu/bdd100k/labels/ins_seg/ins_seg_train.json", "/data5/bdd100k/images/10k/train")
+    register_coco_instances("bdd100k_test", {}, "/shared/xudongliu/bdd100k/labels/ins_seg/ins_seg_val.json", "/data5/bdd100k/images/10k/val")
+
+    if args.eval_only:
+        model = Trainer.build_model(cfg)
+        AdetCheckpointer(model, save_dir=cfg.OUTPUT_DIR).resume_or_load(
+            cfg.MODEL.WEIGHTS, resume=args.resume
+        )
+        res = Trainer.test(cfg, model)
+        # return res
+        if comm.is_main_process():
+            verify_results(cfg, res)
+        if cfg.TEST.AUG.ENABLED:
+            res.update(Trainer.test_with_TTA(cfg, model))
+        return res
+
+    trainer = Trainer(cfg)
+    trainer.resume_or_load(resume=args.resume)
+    return trainer.train()
+
+
+if __name__ == "__main__":
+    # dataset registration
+    # global DatasetCatalog, MetadataCatalog
+
+
+    args = default_argument_parser().parse_args()
+    print("Command Line Args:", args)
+    launch(
+        main,
+        args.num_gpus,
+        num_machines=args.num_machines,
+        machine_rank=args.machine_rank,
+        dist_url=args.dist_url,
+        args=(args,),
+    )
