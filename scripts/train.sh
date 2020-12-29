d31fc8b16a563efaf1672a159828c177ce92f7d4
Xudong Liu <xudongliu@XudongdeMacBook-Pro.local>
2020-12-29 15:32:36 +0800

    cc

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,2 +1,2 @@
 export CUDA_VISIBLE_DEVICES=2,3
-python ./tools/train_bdd.py --num-gpus 2 --dist-url tcp://0.0.0.0:12340  --resume ./out/solov2_r_50_3x/last_checkpoint
+python ./tools/train_bdd.py --num-gpus 2 --dist-url tcp://0.0.0.0:12340  --resume
