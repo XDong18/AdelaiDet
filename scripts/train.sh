f34090596fa1786969b8c90deb3d8830d1523683
XDong18 <liuxd1215@gmail.com>
2021-01-11 16:44:31 +0800

    cc

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,2 +1,2 @@
 export CUDA_VISIBLE_DEVICES=0,1,2,3,8,9
-python ./tools/train_bdd.py --num-gpus 6 --dist-url tcp://0.0.0.0:12340
+python ./tools/train_bdd.py --num-gpus 6 --dist-url tcp://0.0.0.0:12340  --resume
