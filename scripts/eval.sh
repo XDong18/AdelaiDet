871fbdbd6789ea452211c8d72e96460f6df77fa1
XDong18 <liuxd1215@gmail.com>
2021-01-13 16:36:33 +0800

    cc

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -1,2 +1,2 @@
 export CUDA_VISIBLE_DEVICES=1,8,9
-python ./tools/train_bdd.py --num-gpus 3 --dist-url auto --eval-only MODEL.WEIGHTS ./out/solov2_r_101_3x_multi_scale_16bs/model_0012499.pth
+python ./tools/train_bdd.py --num-gpus 3 --dist-url auto --eval-only MODEL.WEIGHTS ./out/CondInst_r_101_3x_multi_scale_6bs_gradient_clip/model_0116499.pth
