c61a123f85271cba036c10eb796315a1e391987b
XDong18 <liuxd1215@gmail.com>
2021-02-10 14:56:03 +0800

    cc

--- tools/train_bdd_catch.py
+++ tools/train_bdd_catch.py
@@ -134,7 +134,7 @@ def setup(args):
     # cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-Detection/faster_rcnn_X_101_32x8d_FPN_3x.yaml")  # Let training initialize from model zoo
 
 
-    cfg.OUTPUT_DIR = './out/CondInst_R_101_3x_sem_bdd100k_single_scale_catch_3'
+    cfg.OUTPUT_DIR = './out/CondInst_R_101_3x_sem_bdd100k_single_scale_catch_4'
     os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
     # new added solver arguments
     cfg.SOLVER.CHECKPOINT_PERIOD = 500
