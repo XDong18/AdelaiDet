697d8981c149f9c3d56f62cacaf4f79f82dd92dc
XDong18 <liuxd1215@gmail.com>
2021-02-10 14:49:31 +0800

    cc

--- vis/demo.py
+++ vis/demo.py
@@ -7,7 +7,7 @@ import time
 import cv2
 import tqdm
 
-from detectron2.config import get_cfg
+from adet.config import get_cfg
 from detectron2.data.detection_utils import read_image
 from detectron2.utils.logger import setup_logger
 
